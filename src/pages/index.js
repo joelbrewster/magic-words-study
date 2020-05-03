45ffc758236db45fdbb1e5ebb19e6f9274f5ceef
Joel Brewster <hi@Joelbrewster.com>
2020-05-04 00:30:31 +1000

    Added header

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,17 +3,19 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
+import Header from "../components/header"
 import SEO from "../components/seo"
 import Magicwordgroup from "../components/magic-word-group"
 
 const IndexPage = () =>  {
     return (
-        <Layout>
+        <>
           <SEO title="Home" />
+          <Header/>
           <div className="grid">
             <Magicwordgroup/>
           </div>
-        </Layout>
+        </>
     )
 }
 
