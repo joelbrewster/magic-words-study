0a6de0f7a3c15bfb5bf22f1dec3b33212fc73f18
Joel Brewster <hi@Joelbrewster.com>
2020-05-04 00:31:19 +1000

    word page template

--- src/pages/golden.js
+++ src/pages/golden.js
@@ -1,7 +1,27 @@
-import React from "react"
+import React, { useState } from 'react'
 import Layout from "../components/layout"
+import EmblaCarouselReact from 'embla-carousel-react'
 
 const IndexPage = () =>  {
+
+  const colour = 'gold';
+  const [embla, setEmbla] = useState(null);
+
+  const options = {
+    align: 'center',
+    containerSelector: '*',
+    slidesToScroll: 1,
+    containScroll: false,
+    draggable: true,
+    dragFree: false,
+    loop: false,
+    speed: 10,
+    startIndex: 0,
+    selectedClass: 'is-selected',
+    draggableClass: 'is-draggable',
+    draggingClass: 'is-dragging'
+  };
+
   const words = [
     'a',
     'and',
@@ -18,18 +38,23 @@ const IndexPage = () =>  {
   ];
   return (
     <>
-	{words.map(word =>
-	  <div className="slide" id={word}>
-	      <div className="col">
-		  <div className="col-container">
-		      <h2>
+	<EmblaCarouselReact
+	  htmlTagName="div"
+	  emblaRef={setEmbla}
+	  options={options}
+	>
+
+	    <div className="slide">
+		{words.map(word =>
+		  <div className="slide-inner"  style={{ backgroundColor: `${colour}` }}>
+		      <p>
 			  {word}
-		      </h2>
+		      </p>
 		  </div>
-		  
-	      </div>
-	  </div>
-	)}
+		)}
+	    </div>
+	</EmblaCarouselReact>
+
     </>
   )
 }
