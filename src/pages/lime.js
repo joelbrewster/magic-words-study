712e1ecd11af181eecdac75ba288fd5626e8dd3a
Joel Brewster <hi@Joelbrewster.com>
2020-05-04 20:45:51 +1000

    lime

---
+++ src/pages/lime.js
@@ -0,0 +1,70 @@
+import React, { useState } from 'react'
+import Layout from "../components/layout"
+import EmblaCarouselReact from 'embla-carousel-react'
+
+const IndexPage = () =>  {
+
+  const colour = '#9FCE62';
+  const [embla, setEmbla] = useState(null);
+
+  const options = {
+    align: 'center',
+    containerSelector: '*',
+    slidesToScroll: 1,
+    containScroll: false,
+    draggable: true,
+    dragFree: false,
+    loop: false,
+    speed: 10,
+    startIndex: 0,
+    selectedClass: 'is-selected',
+    draggableClass: 'is-draggable',
+    draggingClass: 'is-dragging'
+  };
+
+  const words = [
+    "another",
+    "bad",
+    "black",
+    "don't",
+    "father",
+    "found",
+    "got",
+    "head",
+    "know",
+    "live",
+    "may",
+    "once",
+    "put",
+    "run",
+    "should",
+    "tell",
+    "three",
+    "under",
+    "white",
+    "would",
+  ];
+  return (
+    <>
+	<EmblaCarouselReact
+	  htmlTagName="div"
+	  emblaRef={setEmbla}
+	  options={options}
+	>
+
+	    <div className="slide">
+		{words.map(word =>
+		  <div className="slide-inner" style={{ backgroundColor: `${colour}` }}>
+		      <p>
+			  {word}
+		      </p>
+		  </div>
+		)}
+	    </div>
+	</EmblaCarouselReact>
+
+    </>
+  )
+}
+
+export default IndexPage
