1cce105ab72612a3cc7d337b5928a6be3d169f2b
Joel Brewster <hi@Joelbrewster.com>
2020-05-04 21:42:22 +1000

    Updated info

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Magic Words`,
+    description: `Magic words for children to study`,
+    author: `joelbrewster`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
