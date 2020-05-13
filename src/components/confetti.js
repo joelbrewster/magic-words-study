306754135e2127a96e0ec70be69c5911dbe6d1c8
Joel Brewster <joel@iMac.hub>
2020-05-13 20:41:59 +1000

    down to 50 confetti

--- src/components/confetti.js
+++ src/components/confetti.js
@@ -7,36 +7,6 @@ const Confetti = () =>  {
     <>
 	<div className="wrapper">
 	    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512.004 512.004"><path d="M450.656 2.938A8.305 8.305 0 00444.317 0h-27.693c-4.589 0-8.308 3.719-8.308 8.308s3.719 8.308 8.308 8.308h17.885l-19.022 114.135c-5.89 35.334-39.824 64.082-75.646 64.082h-83.84v16.616h83.84c43.583 0 84.869-34.974 92.035-77.965l20.635-123.81a8.305 8.305 0 00-1.855-6.736z" fill="#f5ac51"/><path d="M172.163 194.833c-35.822 0-69.756-28.747-75.646-64.082L77.496 16.616h17.885c4.589 0 8.308-3.719 8.308-8.308S99.971 0 95.382 0H67.688a8.307 8.307 0 00-8.195 9.674l20.635 123.81c7.166 42.991 48.452 77.965 92.035 77.965h83.84v-16.616h-83.84z" fill="#ffd159"/><path fill="#f5ac51" d="M239.387 307.395v58.645h33.232v-58.645l-16.616-11.077z"/><path fill="#f1866d" d="M239.383 249.727h33.232v57.668h-33.232z"/><path d="M339.082 8.308l-83.08 265.855c39.601 0 77.33-31.96 83.84-71.023L372.314 8.308h-33.232z" fill="#f5ac51"/><path d="M315.888 203.14L339.082 8.308H139.691l32.472 194.832c6.51 39.063 44.238 71.023 83.84 71.023 28.287 0 55.234-31.96 59.885-71.023z" fill="#ffd159"/><path d="M256.003 340.302l16.616 72.003 33.232-22.155c-.001-27.531-22.318-49.848-49.848-49.848z" fill="#f5ac51"/><path d="M256.003 340.302c-27.53 0-49.848 22.317-49.848 49.848l66.464 22.155V390.15c0-27.531-7.44-49.848-16.616-49.848z" fill="#ffd159"/><path d="M305.85 390.15h-33.232l44.309 66.464h33.232v-22.155c.001-24.471-19.838-44.309-44.309-44.309z" fill="#f1866d"/><path d="M272.619 390.15h-66.464c-24.471 0-44.309 19.838-44.309 44.309v22.155h155.082v-22.155c0-24.471-19.839-44.309-44.309-44.309z" fill="#f5ac51"/><path fill="#164370" d="M350.16 434.459l-22.155 38.77L350.16 512h33.232v-77.541z"/><path fill="#276aad" d="M128.61 434.463h221.546v77.541H128.61z"/><g fill="#f5ac51"><circle cx="228.306" cy="49.848" r="8.308"/><circle cx="195.074" cy="83.08" r="8.308"/><path d="M375.084 16.616H136.921c-4.589 0-8.308-3.719-8.308-8.308S132.332 0 136.921 0h238.162c4.589 0 8.308 3.719 8.308 8.308s-3.718 8.308-8.307 8.308z"/></g><path d="M275.388 315.703h-38.771c-4.589 0-8.308-3.719-8.308-8.308s3.719-8.308 8.308-8.308h38.771c4.589 0 8.308 3.719 8.308 8.308s-3.719 8.308-8.308 8.308z" fill="#ffd159"/></svg>
-	    <div className="confetti-80"></div>
-	    <div className="confetti-79"></div>
-	    <div className="confetti-78"></div>
-	    <div className="confetti-77"></div>
-	    <div className="confetti-76"></div>
-	    <div className="confetti-75"></div>
-	    <div className="confetti-74"></div>
-	    <div className="confetti-73"></div>
-	    <div className="confetti-72"></div>
-	    <div className="confetti-71"></div>
-	    <div className="confetti-70"></div>
-	    <div className="confetti-69"></div>
-	    <div className="confetti-68"></div>
-	    <div className="confetti-67"></div>
-	    <div className="confetti-66"></div>
-	    <div className="confetti-65"></div>
-	    <div className="confetti-64"></div>
-	    <div className="confetti-63"></div>
-	    <div className="confetti-62"></div>
-	    <div className="confetti-61"></div>
-	    <div className="confetti-60"></div>
-	    <div className="confetti-59"></div>
-	    <div className="confetti-58"></div>
-	    <div className="confetti-57"></div>
-	    <div className="confetti-56"></div>
-	    <div className="confetti-55"></div>
-	    <div className="confetti-54"></div>
-	    <div className="confetti-53"></div>
-	    <div className="confetti-52"></div>
-	    <div className="confetti-51"></div>
 	    <div className="confetti-50"></div>
 	    <div className="confetti-49"></div>
 	    <div className="confetti-48"></div>
