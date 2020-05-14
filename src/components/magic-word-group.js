0b4c1848282dd8b8d5b9fb1e37a1b728ea1f4cc1
Joel Brewster <joel@iMac.hub>
2020-05-14 21:54:06 +1000

    Added key

--- src/components/magic-word-group.js
+++ src/components/magic-word-group.js
@@ -1,22 +1,20 @@
 import { Link } from "gatsby"
-import PropTypes from "prop-types"
 import React from "react"
 
 const Magicwordgroup = () =>  {
   const colours = ['golden', 'red', 'blue', 'green', 'orange', 'indigo', 'violet', 'pink', 'purple', 'aqua', 'lime', 'lemon'];
   return (
     <>
-    {colours.map(colour =>
-      /* lemon isn't a css background colour so set in layout.css */
-      <div className="col" id={colour}>
-          <Link to={colour}>
-	      <div className="col-container">
-		  <p>
-		      {colour}
-		  </p>
-	      </div>
-          </Link>
-      </div>
+	{colours.map((colour, index) =>
+	  <div className="col" id={colour} key={index}>
+              <Link to={colour}>
+		  <div className="col-container">
+		      <p>
+			  {colour}
+		      </p>
+		  </div>
+              </Link>
+	  </div>
     )}
     </>
   )
