bd2bd83261fc744f3909d934e8ba59e6827a2785
Joel Brewster <hi@joelbrewster.com>
2020-06-07 20:06:53 +1000

    Rename manifest from starter for iOS saving webpage

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,8 +18,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Magic-Words`,
+        short_name: `Magic Words`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
