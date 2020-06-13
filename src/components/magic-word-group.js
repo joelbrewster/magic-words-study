4b1a782bcfae452a50c22dd99505729a259f2b36
Joel Brewster <hi@joelbrewster.com>
2020-06-13 20:58:41 +1000

    Added slider arrows for color pages

--- src/components/magic-word-group.js
+++ src/components/magic-word-group.js
@@ -2,21 +2,21 @@ import { Link } from "gatsby"
 import React from "react"
 
 const Magicwordgroup = () =>  {
-  const colours = ['golden', 'red', 'blue', 'green', 'orange', 'indigo', 'violet', 'pink', 'purple', 'aqua', 'lime', 'lemon'];
-  return (
-    <>
-	{colours.map((colour, index) =>
-	  <div className="col" id={colour} key={index}>
-              <Link to={colour}>
-		  <div className="col-container">
-		      <p>
-			  {colour}
-		      </p>
-		  </div>
-              </Link>
-	  </div>
-    )}
-    </>
-  )
+    const colours = ['golden', 'red', 'blue', 'green', 'orange', 'indigo', 'violet', 'pink', 'purple', 'aqua', 'lime', 'lemon'];
+    return (
+        <>
+	  {colours.map((colour, index) =>
+	               <div className="col" id={colour} key={index}>
+                         <Link to={colour}>
+		           <div className="col-container">
+		             <p>
+			       {colour}
+		             </p>
+		           </div>
+                         </Link>
+	               </div>
+                      )}
+        </>
+    )
 }
 export default Magicwordgroup
