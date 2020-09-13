8ffa2763beb424d5f8114b2193fae26d93169aeb
Joel Brewster <joel@BirthdayBook-Pro.hub>
2020-09-13 20:46:28 +1000

    top padding

--- src/components/magic-word-group.js
+++ src/components/magic-word-group.js
@@ -4,7 +4,7 @@ import React from "react"
 const Magicwordgroup = () =>  {
     const colours = ['golden', 'red', 'blue', 'green', 'orange', 'indigo', 'violet', 'pink', 'purple', 'aqua', 'lime', 'lemon', 'tricky1', 'tricky2', 'tricky3', 'tricky4', 'tricky5', 'tricky6'];
     return (
-        <div style={{paddingTop: `100px`}}>
+        <div>
           {colours.map((colour, index) =>
               <div className="col" id={colour} key={index}>
                 <Link to={colour}>
